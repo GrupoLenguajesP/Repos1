7e7ef7aee818e52705eecfd8bdea71b0a8667665
Paola Olmedo <paoolmedov@hotmai.com>
2023-07-04 14:34:50 -0500

    Proyecto Final

---
+++ PantallaDetalle/PantallaDetalle/PantallaDetalle/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PantallaDetalle
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //este va a ser el menu lateral el del lado izquierodo
+            this.Master= new Master();
+
+            //y el del lado derecho
+            //este me va a almacenar cmo primera venbtana un new detalle
+            this.Detail = new NavigationPage(new Detalle());
+
+        }
+    }
+}
